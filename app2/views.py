fed4e65421b1d614b0c9b19106d097e59b55ff3e
Diego_M <diego.monteleone@inacapmail.cl>
2026-09-14 14:54:59 -0300

    proyecto creado

--- app2/views.py
+++ app2/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def app2v1(request):
-    return render(request,"app2/v1.html")
+    return render(request,"app2/app2v1.html")
 
 def app2v2(request):
-    return render(request,"app2/v2.html")
+    return render(request,"app2/app2v2.html")
